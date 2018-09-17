48b964e62a5ea15e5097e4b705d0f0dc8fdf04b1
Kevin Vitale <kevinvitale@gmail.com>
2018-09-17 15:40:12 -0400

    [App] update comment-style for 'AppDelegate.swift'

--- Sources/App/AppDelegate.swift
+++ Sources/App/AppDelegate.swift
@@ -1,32 +1,23 @@
 #if canImport(AppKit)
 import AppKit
 
-/**
- The application's designated delegate.
-     
- - note: This is entirely optional, since we're creating the `NSApp` instance
-         ourselves in `main.swift`.
- */
-public final class AppDelegate: NSObject, NSApplicationDelegate
-{
-    /**
-     Informs the application it should close after the last window is closed.
-
-     - parameter sender: The application.
-     - returns: Always `true`.
-     */
+public final class AppDelegate: NSObject, NSApplicationDelegate {
+    /// Informs the application it should close after the last window is closed.
+    ///
+    /// - Parameters:
+    ///     - sender: The application.
+    ///
+    /// - Returns: Always `true`.
     public func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
 }
 
-extension AppDelegate
-{
-    /**
-     Assigns `app.delegate` to `self`.
-     
-     - parameter app: The application which is to use the receiver as its delegate.
-     */
+extension AppDelegate {
+    /// Assigns `app.delegate` to `self`.
+    ///
+    /// - Parameters:
+    ///     - app: The application which is to use the receiver as its delegate.
     convenience init(with app: NSApplication) {
         self.init()
         app.delegate = self
